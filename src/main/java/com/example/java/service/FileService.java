dbfaab9fc100f3a293b9e0fee3a7759e2bcb6eb5
Edmond.Hyka <edmond.hyka@fshnstudent.info>
2024-05-26 16:23:36 +0300

    missing file

--- src/main/java/com/example/java/service/FileService.java
+++ src/main/java/com/example/java/service/FileService.java
@@ -1,2 +1,14 @@
-package com.example.java.service;public interface FileService {
+package com.example.java.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+
+public interface FileService {
+
+    HashSet<String> getFileWithQuery(MultipartFile file, String id) throws IOException;
+
+    HashSet<String> getFileWithCobol(MultipartFile file) throws IOException;
 }
