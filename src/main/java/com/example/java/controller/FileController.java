076cdba416494bd0c7c4af8fc91d68ec94b708a6
Edmond.Hyka <edmond.hyka@fshnstudent.info>
2024-05-26 16:29:05 +0300

    missing file

--- src/main/java/com/example/java/controller/FileController.java
+++ src/main/java/com/example/java/controller/FileController.java
@@ -1,54 +1,33 @@
-package com.example.java;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+package com.example.java.controller;
+
+import com.example.java.service.FileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+
 
 @RestController
+@RequestMapping("/api/file")
 public class FileController {
 
-    @PostMapping("/processFileWithQuery")
-    public List<String> processFile(@RequestParam("file") MultipartFile file,
-                                    @RequestParam("id") String id) throws IOException {
-        List<String> linesWithId = new ArrayList<>();
-        List<String> sqlQueries = new ArrayList<>();
+    private FileService fileService;
 
-        // check about the format of the file
-        if (!isValidFileFormat(file)) {
-            throw new IllegalArgumentException("The file format is incorrect. Allowed format: .log");
-        }
-        if (!file.isEmpty()) {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    if (line.contains(id)) {
-                        linesWithId.add(line);
-                    }
-                }
-            }
-        }else throw new RuntimeException("The file cannot be empty");
-        // Iterate through the list of rows with the given id
-        for (String line : linesWithId) {
-            if (line.contains("Parsing final sqlString")) {
-                sqlQueries.add(line);
-            }
-        }
-
-        return sqlQueries;
-
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
     }
 
-    public static boolean isValidFileFormat(MultipartFile file) {
-        if (file == null) {
-            return false;
-        }
-        // Checks if the file has a .log extension
-        String fileName = file.getOriginalFilename();
-        return fileName != null && fileName.toLowerCase().endsWith(".log");
+    @PostMapping("/getResultFromQuery")
+    public ResponseEntity<HashSet<String>> processFile(@RequestParam("file") MultipartFile file,
+                                                       @RequestParam("id") String id) throws IOException {
+        return ResponseEntity.ok(fileService.getFileWithQuery(file, id));
     }
+
+    @PostMapping("/getResultFromCoobol")
+    public ResponseEntity<HashSet<String>> processFile(@RequestParam("file") MultipartFile file) throws IOException {
+        return ResponseEntity.ok(fileService.getFileWithCobol(file));
+    }
+
 }
